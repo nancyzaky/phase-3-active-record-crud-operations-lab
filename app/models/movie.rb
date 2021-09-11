6635770767244556e645778a644d15c9394093c0
Nancy Zaky <nancyzki@yahoo.com>
2021-09-11 16:11:41 -0700

    commit tree 0635746d8d98e964a886bd0ff6146851409d7ad0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Movie < ActiveRecord::Base
+  def self.create_with_title(title)
+  Movie.create(title: title)
+  end
 
+  def self.first_movie
+  Movie.first
+  end
+
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_count
+    Movie.count
+  end
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.find_movie_with_attributes(attrs)
+  Movie.find_by(attrs)
+  end
+
+  def self.find_movies_after_2002
+    Movie.where('release_date > 2002')
+  end
+
+  def update_with_attributes(attrs)
+    self.update(attrs)
+
+  end
+  def self.update_all_titles(title)
+    self.update(title:title)
+  end
+  def self.delete_by_id(id)
+    self.destroy(id)
+  end
+
+  def self.delete_all_movies
+    self.destroy_all
+  end
 end
